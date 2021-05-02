5f0fe5b20581cd9a3bcc2cf0c4fffe549c0f9606
Matheus Wallace <matheuswts2011@hotmail.com>
2021-05-02 17:06:14 -0300

    update asdf script

--- scripts/programs/asdf.sh
+++ scripts/programs/asdf.sh
@@ -5,6 +5,6 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
 
 # Install nodejs with asdf
 figlet Installing NodeJS | lolcat
-~/.asdf/bin/asdf plugin-add nodejs
+~/.asdf/bin/asdf plugin add nodejs
 ~/.asdf/bin/asdf install nodejs latest:lts
 ~/.asdf/bin/asdf global nodejs lts
