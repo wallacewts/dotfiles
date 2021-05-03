3b567c97538afd61b24acfe9f6627f79e51ea4b5
Matheus Wallace <matheuswts2011@hotmail.com>
2021-05-02 21:18:40 -0300

    update zsh.sh

--- scripts/programs/zsh.sh
+++ scripts/programs/zsh.sh
@@ -7,5 +7,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 figlet Installing spaceship theme | lolcat
 sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
