c7668d05879b2fc10268b0caa1b9028cc17d9109
Matheus Wallace <matheuswts2011@hotmail.com>
2021-05-04 19:43:46 -0300

    update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -35,16 +35,9 @@ for f in programs/*.sh; do bash "$f" -H; done
 # Copy dotfiles
 ./copy.sh
 
-zsh
-figlet Installing spaceship theme | lolcat
-sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
 # Get all upgrades
 sudo apt upgrade -y
 sudo apt autoremove -y
 
 # Fun finish message
 figlet Finish installation | lolcat
-
--- scripts/programs/zsh.sh
+++ scripts/programs/zsh.sh
@@ -4,3 +4,8 @@ figlet Installing ZSH | lolcat
 sudo apt install -y zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+figlet Installing spaceship theme | lolcat
+sudo /bin/zsh -i -c "git clone https://github.com/denysdovhan/spaceship-prompt.git '$ZSH_CUSTOM/themes/spaceship-prompt'"
+sudo /bin/zsh -i -c "ln -s '$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme' '$ZSH_CUSTOM/themes/spaceship.zsh-theme'"
+sudo /bin/zsh -i -c "sh -c '$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)'"
