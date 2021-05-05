0f063195b06e7c2a1bbfca830bd2b8fba541d53e
Matheus Wallace <matheuswts2011@hotmail.com>
2021-05-04 22:04:53 -0300

    update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -5,6 +5,6 @@ cd ..
 
 cp -r .config ${HOME}
 cp -r .fonts ${HOME}
-ln -s "$(pwd)/.gitconfig" ~/.zshrc
+ln -s "$(pwd)/.gitconfig" ~/.gitconfig
 ln -s "$(pwd)/.zshrc" ~/.zshrc
-ln -s "$(pwd)/.bashrc" ~/.zshrc
+ln -s "$(pwd)/.bashrc" ~/.bashrc
