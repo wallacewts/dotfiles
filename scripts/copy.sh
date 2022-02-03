e3eac42053f571072b9e4048c41f10f7c2af6fa2
Matheus Wallace <32605520+wallacewts@users.noreply.github.com>
2022-02-03 19:10:24 -0300

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -3,8 +3,14 @@
 # Up from scripts dir
 cd ..
 
+# remove files if already exists
+rm -rf "$(pwd)/.gitconfig" "$(pwd)/.zshrc" "$(pwd)/.bashrc"
+
+# copy dot folders
 cp -r .config ${HOME}
 cp -r .fonts ${HOME}
+
+# create link for dotfiles
 ln -s "$(pwd)/.gitconfig" ~/.gitconfig
 ln -s "$(pwd)/.zshrc" ~/.zshrc
 ln -s "$(pwd)/.bashrc" ~/.bashrc
