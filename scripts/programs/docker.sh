c67043e0b65c367a5cb4c5b9045c2c9bb7f1c5a4
Matheus Wallace <32605520+wallacewts@users.noreply.github.com>
2022-02-03 19:18:52 -0300

    Update docker.sh

--- scripts/programs/docker.sh
+++ scripts/programs/docker.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
+# docker
 figlet Installing Docker | lolcat
-sudo apt update
-sudo apt-get install -y \
-    apt-transport-https \
+
+sudo apt-get update
+
+sudo apt-get install \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-#sudo groupadd docker
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ 
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo docker run hello-world
+
+if [ ! $(getent group docker) ]; then
+    sudo groupadd docker
+fi
+
 sudo usermod -aG docker $USER
-#newgrp docker 
+
+newgrp docker
+
 docker run hello-world
 
+# docker-compose
 figlet Installing Docker Compose | lolcat
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 sudo chmod +x /usr/local/bin/docker-compose
